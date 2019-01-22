597f68c289c93801e7814828a59a19c7a2bcd8dc
tpgmartin <tpgmartin@gmail.com>
2019-01-22 17:18:21 +0000

    Outline script to train classifiers

---
+++ models/train.py
@@ -0,0 +1,25 @@
+# TODO: load dependencies
+
+if __name__ == "__main__":
+
+    # TODO: create HDFS instance
+
+    # what is difference between `SparkConf` and `SparkContext` objects?
+    # TODO: create SparkConf object
+    # TODO: create SparkContext object
+
+    # TODO: load positive and negative sentiment datasets separately
+
+    # TODO: create list combining positive and negative tweets, using binary 
+    # numeric label to indicate if sentiment is negative or positive
+
+    # TODO: create RDD of combine list, and perform tokenisation. Only keep 
+    # certain word tokens
+
+    # TODO: create hash table
+
+    # TODO: create training data with `LabelePoint` and hash of text tokens
+
+    # TODO: perform test train split
+
+    # For each model train, calculate error, and save to directory
